eacf636d7eb3f30f21dccdd11eb62f4a4ce01b07
Ryosuke MURASE <31121735+MuraseRyosuke@users.noreply.github.com>
2026-01-31 01:09:17 +0900

    Add files via upload

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,1 +1,5 @@
+/**
+ * OneSignal Service Worker Entry Point
+ * This file is required to enable push notifications.
+ */
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
